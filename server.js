6b56165f902aa859f892f519774ae5b453792c8d
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-02-10 18:45:04 -0600

    update port and export app

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 7673;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -9,3 +9,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+module.exports = app;
