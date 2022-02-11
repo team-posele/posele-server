bd694bb17dc644ef3345808e0ea23d9b1148963d
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-02-10 19:06:36 -0600

    initial routes and port set for deployment

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 7673;
+const port = process.env.PORT || 7673;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/postImage", (req, res, next) => {
+  res.status(200).send("reaching post route. nothing posted yet.");
 });
 
 app.listen(port, () => {
